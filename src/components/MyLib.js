afdc6e3fc327d4939fbb4b1bcea1275a42a83ca8
le hoang nam <lehoangnam.31123@gmail.com>
2024-10-17 11:19:55 +0700

    Update: Hanlde header page - success

--- src/components/MyLib.js
+++ src/components/MyLib.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Header from './Header';
+import '../../src/MyLib.css';
 
 class MyLib extends React.Component {
     render() {
-        return (
+        return (            
             <Header />
         );
     }
